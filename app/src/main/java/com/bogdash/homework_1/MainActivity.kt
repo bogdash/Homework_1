183d9e6cfec1fa2c7254b873db9ea26ebcaa7ff5
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-04-10 00:22:06 +0300

    Add secret key exchange between applications

--- app/src/main/java/com/bogdash/homework_1/MainActivity.kt
+++ app/src/main/java/com/bogdash/homework_1/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.bogdash.homework_1
 
+import android.database.Cursor
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +15,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val buttonGetSecretKey: Button = findViewById(R.id.buttonGetSecretKey)
+        buttonGetSecretKey.setOnClickListener {
+            getSecretKey()
+        }
+    }
+
+    private fun getSecretKey() {
+        val uri = Uri.parse("content://dev.surf.android.provider/text")
+        val contentResolver = contentResolver
+        val cursor: Cursor? = contentResolver.query(uri, null, null, null, null)
+        cursor?.use {
+            if (it.moveToFirst()) {
+                val columnIndex = it.getColumnIndex("text")
+                if (columnIndex != -1) {
+                    val text = it.getString(columnIndex)
+                    Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Секретный ключ не найден", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                Toast.makeText(this, "Данные не найдены", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
